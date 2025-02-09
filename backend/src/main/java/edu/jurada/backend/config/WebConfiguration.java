8ee35017b33e301d64275cc0ac6b5cda5cbcedb5
Adam Jurkiewicz <s25467@pjwstk.edu.pl>
2025-02-09 20:56:46 +0100

    Add cors origin

--- backend/src/main/java/edu/jurada/backend/config/WebConfiguration.java
+++ backend/src/main/java/edu/jurada/backend/config/WebConfiguration.java
@@ -1,14 +1,20 @@
 package edu.jurada.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+	@Value("${cors.origin:http://localhost:5173}")
+	private String corsOrigin;
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("*");
+		registry
+				.addMapping("/**")
+				.allowedMethods("*")
+				.allowedOrigins(corsOrigin);
 	}
 }
